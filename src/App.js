0f78e799e4ab26b39a2a2ec7ac23fc745ca14237
Juhun Kwak <hn04193@naver.com>
2020-09-17 00:41:30 +0900

    0917_01

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class Navigation extends Component{
   render(){
+    var list = [];
+    var i = 0;
+    while(i < this.props.data.length){
+      var data = this.props.data[i];
+      list.push(
+        <li key={data.id}>
+          <a href={data.id+'.html'} onClick={function(id, e){
+            e.preventDefault(); // 페이지 전환 막기
+            this.props.onSelect(id);
+          }.bind(this, data.id)}>
+            {data.title}
+          </a>
+        </li>)
+      i = i + 1;
+    }
     return(
       <nav>
         <ol>
-          <li><a href="1.html">HTML</a></li>
-          <li><a href="2.html">CSS</a></li>
-          <li><a href="3.html">JavaScript</a></li>
+          {list}
         </ol>
       </nav>
     )
@@ -29,29 +41,123 @@ class Body extends Component{
   render(){
     return(
       <article>
-        <h2>{this.props.id}</h2>
-        {this.props.title}
-        {this.props.desc}
+        <h2>{this.props.data.title}</h2>
+        {this.props.data.desc}
       </article>
     )
   }
 }
-class App extends Component{
+class ContentCreate extends Component{
   state = {
+    title:'',
+    desc:''
+  }
+  changeFormHandler(e){
+    this.setState({[e.target.name]:e.target.value});
+          // name에 따라 값이 달라짐
+  }
+  render(){
+    return (
+      <article>
+        <form onSubmit={function(e){
+          e.preventDefault();
+          this.props.onSubmit(this.state);
+        }.bind(this)}>
+          <p><input type="text" placeholder="title" name="title"
+           value={this.state.title} onChange={this.changeFormHandler.bind(this)}></input></p>
+          <p><textarea placeholder="description" name="desc"
+           value={this.state.desc} onChange={this.changeFormHandler.bind(this)}></textarea></p>
+          <p><input type="submit"></input></p>
+        </form>
+      </article>
+    )
+  }
+
+}
+class App extends Component{
+  last_content_id=3;
+  state = {
+    mode:'read',
+    selected_content_id:1,
     contents:[
       {id:1, title:'HTML', desc:'HTML is for infomation'},
       {id:2, title:'CSS', desc:'CSS is for infomation'},
       {id:3, title:'JavaScript', desc:'Javascript is for infomation'},
     ]
   }
+  getSelectedContent(){
+    var i = 0;
+    while(i < this.state.contents.length){
+      var data = this.state.contents[i];
+      if(this.state.selected_content_id === data.id){
+        return data;
+      }
+      i = i + 1;
+    }
+  }
+  getContentComponent(){
+    if(this.state.mode === 'read'){
+      return <Body data={this.getSelectedContent()}></Body>
+    } else if(this.state.mode === 'welcome'){
+      return <Body data={{
+        title:"main",
+        desc:'Hello',
+      }}></Body>
+    } else if(this.state.mode === 'create'){
+      return <ContentCreate onSubmit={function(formData){
+        console.log(formData)
+        this.last_content_id = this.last_content_id + 1;
+        formData.id = this.last_content_id;
+        var newContents = Object.assign([], this.state.contents);
+        newContents.push(formData);
+        this.setState({
+          contents:newContents,
+          selected_content_id:this.last_content_id,
+          mode:'read'
+
+          });
+      }.bind(this)}></ContentCreate>
+    }
+  }
+  getControlComponent(){
+    return [
+      <a key="1" href="/create" onClick={function(e){
+        e.preventDefault();
+        this.setState({mode:'create'})
+      }.bind(this)}>create </a>,
+      <a key="2" href="/update" onClick={function(e){
+        e.preventDefault();
+      }}>update </a>,
+      <input key="3" type="button" href="/delete" onClick={function(){
+        var newContents = this.state.contents.filter(function(el){
+          if(el.id !== this.state.selected_content_id){
+            return el;
+          }
+        }.bind(this));
+        this.setState({
+          contents:newContents,
+          mode:'welcome'
+        });
+
+      }.bind(this)} value="delete"></input>
+    ];
+  }
   render(){
     return (
       <div className="App">
-        <Header title="Web"></Header>
-        <Body data={this.state.contents}></Body>
-        <Navigation></Navigation>
+        <Header onClick={function(){
+          this.setState({mode:'welcome'});
+        }.bind(this)}
+          title="Web"></Header>
+        <Navigation onSelect={function(id){
+          console.log('App',id);
+          this.setState({selected_content_id:id});
+          // this.state.selected_content_id의 값을 id로 바꿔라
+        }.bind(this)}
+        data={this.state.contents}></Navigation>
+        {this.getControlComponent()}
+        {this.getContentComponent()}
       </div>
-      
     )
   }
 }
